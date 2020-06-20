568b69ece7d13f83edc4098ac0a2c60a8d03dd67
meghaborse <65807572+meghaborse@users.noreply.github.com>
2020-06-20 01:06:09 -0700

    first commit

---
+++ src/app/registration.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {User } from './user';
+import {HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  constructor(private _http : HttpClient) { }
+
+  public loginUserFormRemote(user :User):Observable<any> {
+     return  this._http.post<any>("http://localhost:8080/login",user)
+  }
+}
